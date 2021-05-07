4c79aaa7fec44eadee37418b3b43dad6c3852153
18616682280 <wei.dong120@icloud.com>
2021-05-07 19:03:02 +0800

    init

---
+++ README.md
@@ -0,0 +1,14 @@
+# mac-agent
+
+#### Brief Intro
+{
+it's an excutable agent to inject exposer to target process 
+}
+
+#### Architecture
+TODO
+
+
+#### Features
+
+1. apply bytecode change against target application
---
+++ src/test/java/com/cte4/mac/agent/AgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cte4.mac.agent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
